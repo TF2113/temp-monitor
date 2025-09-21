277015dd880aea72f5e56a81f5532f23d927701c
TF2113 <148654475+TF2113@users.noreply.github.com>
2025-09-21 21:36:21 +0100

    Create Task DTO record file

---
+++ backend/src/main/java/com/tf2113/backend/domain/dto/TaskDto.java
@@ -0,0 +1,13 @@
+package com.tf2113.backend.domain.dto;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record TaskDto(
+        UUID id,
+        Double temperature,
+        Double humidity,
+        Double pressure,
+        LocalDateTime timestamp
+) {
+}
