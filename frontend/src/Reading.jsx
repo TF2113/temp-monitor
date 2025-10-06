720001ac2c594deb5a84d284e2b5b23ee3a01d42
TF2113 <148654475+TF2113@users.noreply.github.com>
2025-10-06 19:07:43 +0100

    Add Z suffix to timestamp

--- frontend/src/Reading.jsx
+++ frontend/src/Reading.jsx
@@ -33,7 +33,7 @@ function Reading(){
                     <p>Temperature: <strong>{reading.temperature} °C</strong></p>  
                     <p>Humidity: <strong>{reading.humidity}%</strong></p>  
                     <p>Pressure: <strong>{reading.pressure} hPa</strong></p>
-                    <p>Taken at: <strong>{new Date(reading.timestamp).toLocaleString("en-GB", {
+                    <p>Taken at: <strong>{new Date(reading.timestamp + "Z").toLocaleString("en-GB", {
                         timeZone: "Europe/London",
                         hour12: false
                     })}</strong></p>   
